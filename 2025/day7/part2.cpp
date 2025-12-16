fdc427ef1be0009defb2adcc8666e2bf3029848f
Murcas <marcusth@gmail.com>
2025-12-16 21:35:20 +0100

    day7 done

--- 2025/day7/part2.cpp
+++ 2025/day7/part2.cpp
@@ -12,6 +12,9 @@ using boost::multiprecision::cpp_int;
 #define EMITTER  'S'
 
 void laserTraversal(int r, int c, std::vector<std::string> &map, cpp_int &paths);
+cpp_int countPaths(int r, int c, std::vector<std::string> &map, std::vector<std::vector<cpp_int>> &memo);
+
+static const cpp_int NoEntry = -1;
 
 int main (int argc, char *argv[]) {
   std::fstream file(argv[1]);
@@ -19,7 +22,6 @@ int main (int argc, char *argv[]) {
     std::cout << "Could not open file" << std::endl;
     return 1;
   }
-
 
  
   std::vector<std::string> map;
@@ -44,12 +46,41 @@ int main (int argc, char *argv[]) {
     return 1;
   }
 
-  cpp_int answer = 0;
 
-  laserTraversal(0, startCol, map, answer);
+  std::vector<std::vector<cpp_int>> memo(mapHeight, std::vector<cpp_int>(mapWidth, NoEntry));
+  //laserTraversal(0, startCol, map, answer);
+
+  cpp_int answer = countPaths(0, startCol, map, memo);
   
+  std::cout << mapWidth << " : " << mapHeight << std::endl;
   std::cout << answer << std::endl;
   return 0;
+}
+
+
+cpp_int countPaths(int r, int c, std::vector<std::string> &map, std::vector<std::vector<cpp_int>> &memo) {
+  const int mapHeight = map.size();
+  const int mapWidth = map[0].size();
+
+  if (c < 0 || c >= mapWidth) return 0;
+  if (r == mapHeight - 1) return 1;
+  if (memo[r][c] != NoEntry) return memo[r][c];
+
+  char below = map[r+1][c];
+  cpp_int res = 0;
+  switch (below) {
+    case EMPTY:
+      res = countPaths(r+1, c, map, memo);
+      break;
+    case SPLITTER:
+      res = countPaths(r+1, c+1, map, memo) + countPaths(r+1, c-1, map, memo);
+    break;
+    default:
+      res = 0;
+    break;
+  }
+  memo[r][c] = res;
+  return res;
 }
 
 void laserTraversal(int r, int c, std::vector<std::string> &map, cpp_int &paths) {
